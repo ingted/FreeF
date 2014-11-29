0d6b4c5632ec4f9f8cbe936f4e29175a5e3f50cd
pocketberserker <pocketberserker@gmail.com>
2014-11-30 00:53:54 +0900

    add tests

---
+++ FreeF.Tests/StrictFreeTest.fs
@@ -0,0 +1,54 @@
+﻿namespace FSharp.FreeF.Tests
+
+open NUnit.Framework
+open FsUnit
+open FreeF.Strict
+
+[<TestFixture>]
+module StrictFreeTest =
+
+  type FreeBuilder () =
+    member this.Return(x) = Free.Trampoline.done_ x
+    member this.ReturnFrom(x) = x
+    member this.Bind(x, f) = Free.bind f x
+
+  let free = FreeBuilder()
+
+  let rec fib n =
+    if n < 2M then Free.Trampoline.done_ n
+    else
+      free {
+        let! x = Free.Trampoline.suspend (fun () -> fib (n - 1M))
+        let! y = Free.Trampoline.suspend (fun () -> fib (n - 2M))
+        return x + y
+      }
+
+  module KF0 = FSharp.Karma.F0
+
+  let run<'X1, 'X2, 'T> f = Free.Trampoline.run KF0.functor_ f
+
+  [<Test>]
+  let ``fib test`` () =
+    fib 6M
+    |> run<decimal, decimal, decimal>
+    |> should equal 8M
+
+  //[<Test>]
+  //let ``more fib test`` () =
+  //  fib 35M
+  //  |> run<decimal, decimal, decimal>
+  //  |> should equal 9227465M
+
+  let rec factorial n =
+    if n < 2M then Free.Trampoline.done_ 1M
+    else
+      free {
+        let! x = Free.Trampoline.suspend (fun () -> factorial (n - 1M))
+        return n * x
+      }
+
+  [<Test>]
+  let ``factorial test`` () =
+    factorial 5M
+    |> run<decimal, decimal, decimal>
+    |> should equal 120M
